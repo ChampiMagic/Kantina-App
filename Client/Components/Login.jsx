134372932226fc5f928e583a6301afa2d17b0672
homero <homerogazze1015@gmail.com>
2022-07-26 17:01:57 -0300

    [NEW] Login Component Created. Not Yet Functional(Only Console.log)

--- Client/Components/Login.jsx
+++ Client/Components/Login.jsx
@@ -1,9 +1,34 @@
 import React from "react";
 import { Formik } from 'formik'
-import CustomTextInput from "./CustomTextInput";
+import FormikTextInput from "./FormikTextInput";
+import { View, Button } from "react-native";
+import { loginValidationSchema } from "../Utils/ValidationSchemas/Login";
 
 export default function Login() {
+
+    const initialValues = {
+        email: '',
+        password: ''
+    }
+
     return (
-        <CustomTextInput />
+        <Formik validationSchema={loginValidationSchema} initialValues={initialValues} onSubmit={(values) => console.log(values)}>
+            {({ handleSubmit }) => {
+                return (
+                    <View>
+                        <FormikTextInput 
+                        name='email'
+                        placeholder='E-mail'
+                        />
+                        <FormikTextInput 
+                        name='password'
+                        placeholder='Password'
+                        secureTextEntry
+                        />
+                        <Button onPress={handleSubmit} title='Sign In' />
+                    </View>
+                )
+            }}
+        </Formik>
     )
 }
