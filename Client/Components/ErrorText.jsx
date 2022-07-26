4f415fab6bedd0559fe7f343813bae5f1f41f9a7
homero <homerogazze1015@gmail.com>
2022-07-26 16:55:09 -0300

    [NEW] ErrorText component Created

---
+++ Client/Components/ErrorText.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    ErrorText: {
+        fontSize: 15,
+        color: '#c91c1c',
+        fontWeight: '500',
+        marginBottom: 10
+    }
+})
+
+export default function ErrorText({children, style, ...props}) {
+
+    const CustomStyles = [
+        styles.ErrorText,
+        style
+    ]
+
+    return (
+        <>
+            <Text style={CustomStyles} {...props}>
+                {children}
+            </Text>
+        </>
+    )
+}
