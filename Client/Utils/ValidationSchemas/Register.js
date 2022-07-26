da108795cd859937a9cafea1221cf98b1e01f7a7
homero <homerogazze1015@gmail.com>
2022-07-26 18:51:47 -0300

    [NEW] Register Validation Schema Created

---
+++ Client/Utils/ValidationSchemas/Register.js
@@ -0,0 +1,18 @@
+import * as yup from 'yup';
+
+
+export const registerValidationSchema = yup.object().shape({
+    email: yup
+    .string()
+    .email()
+    .required('E-mail is requiered'),
+    password: yup
+    .string()
+    .min(5, 'Too short!')
+    .max(1000, 'Too long!')
+    .required('Password is requiered'),
+    passwordConfirmation: yup
+    .string()
+    .required('Please retype your password.')
+    .oneOf([yup.ref('password')], `Your passwords don't match.`)
+})
