6e17e2241c9ebc3a6afb0450575368711c088958
homero <homerogazze1015@gmail.com>
2022-07-29 17:01:18 -0300

    [NEW] New Pages Added. Only Demo Pages

---
+++ Client/Pages/Chronogram.jsx
@@ -0,0 +1,68 @@
+import { View, Text, StatusBar, StyleSheet, ImageBackground, Image } from "react-native";
+import { useDispatch, useSelector } from 'react-redux';
+import { AntDesign } from '@expo/vector-icons'; 
+
+
+const URI = {
+    Login: "https://images.unsplash.com/photo-1528459801416-a9e53bbf4e17?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8YmFja2dyb3VuZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60",
+}
+
+const Chronogram = ({ navigation }) => {
+
+    const count = useSelector(state => state.counter.value)
+    const dispatch = useDispatch()
+
+    return (
+    
+        <ImageBackground source={{ uri: URI.Login }} resizeMode="cover" style={styles.BackgroundImage}>
+           <StatusBar
+            animated={true}
+            barStyle='light-content'
+            backgroundColor="rgba(0, 0, 0, 0.07)"
+            translucent={true}
+            />
+            <View style={styles.imageContainer}>
+                <Image style={{ width: 300, height: 300 }} source={require("../assets/kantinaLogo.png")}/>
+            </View>
+            <View style={styles.subContainer}>
+
+        
+                <View style={styles.iconContainer}>
+                    <AntDesign name="calendar" size={75} color="black" />
+                    <Text style={styles.text}>Calendario</Text>
+                </View>
+
+
+            </View>
+        </ImageBackground>
+    )
+}
+
+export default Chronogram;
+
+const styles = StyleSheet.create({
+    BackgroundImage: {
+        flex:1,
+        alignItems: 'center',
+        justifyContent: 'flex-end'
+    },
+    imageContainer: {
+        flexGrow: 4,
+        paddingTop: 50
+    },
+    subContainer: {
+        flexGrow: 8,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignContent: 'space-between',
+    },
+    iconContainer: {
+        alignItems: 'center',
+        width: '50%',
+        marginBottom: 80,
+    },
+    text: {
+        fontSize: 20,
+        fontWeight: '600'
+    }
+})
