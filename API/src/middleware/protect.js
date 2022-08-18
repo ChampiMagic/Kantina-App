4f76e3c4889a14916ee2e1d6c02093f0157290f8
homero <homerogazze1015@gmail.com>
2022-08-18 12:21:26 -0300

    [NEW] Protect middleware created (JWT for private routes)

---
+++ API/src/middleware/protect.js
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken'
+import 'dotenv/config';
+
+import { errorCreator } from '../../utils/responseCreator.js';
+
+export const protect = (req, res, next) => {
+
+    const { authorization } = req.headers
+    
+    let token = ''
+
+    if(authorization && authorization.toLowerCase().startsWith('bearer')) {
+        token = authorization.substring(7)
+    }
+
+    const decodeToken = jwt.verify(token, process.env.SECRET_WORD)
+
+    if(!token || !decodeToken._id) {
+        next(new errorCreator('token is missing or invalid', 401))
+    }
+
+    req.body.userId = decodeToken._id
+
+    next()
+}
