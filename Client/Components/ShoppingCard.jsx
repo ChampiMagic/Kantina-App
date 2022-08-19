cb8b20b469196e7ea764bd487504a77291c4864c
homero <homerogazze1015@gmail.com>
2022-08-19 17:20:40 -0300

    [UPDATE] ADD Icon added

--- Client/Components/ShoppingCard.jsx
+++ Client/Components/ShoppingCard.jsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
 
+//import icons
+import { AntDesign } from '@expo/vector-icons';
 
 
 const ShoppingCard = ({_id, image, title, price}) => {
@@ -9,6 +11,10 @@ const ShoppingCard = ({_id, image, title, price}) => {
         <View style={styles.cardContainer} key={_id}>
             <View style={styles.imgContainer}>
                 <Image source={{ uri: image }} resizeMode='cover' style={styles.img}/>
+                <TouchableOpacity style={styles.icon}>
+                     <AntDesign  name="pluscircleo" size={30} color="black" />
+                </TouchableOpacity>
+               
             </View>
             <View style={styles.details}>
                 <Text style={styles.price}>$ {price}</Text>
@@ -37,6 +43,11 @@ const styles = StyleSheet.create({
     img: {
         width: '100%',
         height: 100
+    },
+    icon: {
+        position: 'absolute',
+        left: '75%',
+        top: 5,
     },
     details: {
         paddingHorizontal: 10
