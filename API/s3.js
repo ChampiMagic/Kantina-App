daf91c587b54e127ffb7ac14dd313efc233412d7
homero <homerogazze1015@gmail.com>
2022-08-27 13:56:00 -0300

    [NEW] AWS S3 Configuration file && Download/Upload functions created

---
+++ API/s3.js
@@ -0,0 +1,40 @@
+import S3 from 'aws-sdk/clients/s3.js'
+import fs from 'fs'
+import 'dotenv/config'
+
+const bucketName = process.env.AWS_BUCKET_NAME;
+const region = process.env.AWS_BICKET_REGION;
+const accessKeyId = process.env.AWS_ACCESS_KEY;
+const secretAccessKey = process.env.AWS_SECRET_KEY;
+
+const s3 = new S3({
+    region,
+    credentials: {
+        accessKeyId,
+        secretAccessKey
+    }
+})
+
+//upload a file to s3
+export function upload(file) {
+    const fileStream = fs.createReadStream(file.path)
+
+    const uploadParams = {
+        Bucket: bucketName,
+        Body: fileStream,
+        Key: file.filename
+    }
+
+    return s3.upload(uploadParams).promise()
+}
+
+
+//downloads a file from s3
+export function getFileStream(fileKey) {
+    const downloadParams = {
+        Key: fileKey,
+        Bucket: bucketName
+    }
+
+    return s3.getObject(downloadParams).createReadStream()
+}
