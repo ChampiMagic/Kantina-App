664ebc66f5317d05a24f5cb8dec39ace36d9a547
homero <homerogazze1015@gmail.com>
2022-09-01 16:34:39 -0300

    [NEW] useGetUser Hook created (for Qr code scanning)

---
+++ Client/Utils/Hooks/useGetUser.js
@@ -0,0 +1,28 @@
+//import dependencies
+import axios from 'axios'
+
+//import constructor
+import HeaderConstructor from '../Constructors/HeaderConstructor.js';
+
+const useGetUser = async (id) => {
+
+    const config = await HeaderConstructor()
+    
+    try {
+
+        const metaData = await axios.get(`/privateUser/?userId=${id}`, config)
+        const response = metaData.data.response
+
+            console.log("response", response)
+        return { data: response.user , message: response.message,  error: null}
+
+    } catch(err){
+
+        if(err.message = 'Network Error')  return { data: {},  message: null,  error: err.message }
+    
+        return { data: {},  message: null,  error: err.response.data.message }
+    }
+
+}
+
+export default useGetUser;
